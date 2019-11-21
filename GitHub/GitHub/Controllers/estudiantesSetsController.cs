df4ec52224084cff10dff74fb74d92fd6ed78d52
yadhira feliz <yara09feliz@hotmail.com>
2019-11-21 10:32:10 -0400

    Se agregaron los modulos de persona, agenda, y estudiante(para ver el horario)

---
+++ GitHub/GitHub/Controllers/estudiantesSetsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GitHub.Models;
+
+namespace GitHub.Controllers
+{
+    public class estudiantesSetsController : Controller
+    {
+        private GHBbdEntities db = new GHBbdEntities();
+
+        // GET: estudiantesSets
+        public ActionResult Index()
+        {
+            return View(db.estudiantesSet.ToList());
+        }
+
+        // GET: estudiantesSets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estudiantesSet estudiantesSet = db.estudiantesSet.Find(id);
+            if (estudiantesSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantesSet);
+        }
+
+        // GET: estudiantesSets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: estudiantesSets/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Idestudiantes,materia,nombre")] estudiantesSet estudiantesSet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.estudiantesSet.Add(estudiantesSet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(estudiantesSet);
+        }
+
+        // GET: estudiantesSets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estudiantesSet estudiantesSet = db.estudiantesSet.Find(id);
+            if (estudiantesSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantesSet);
+        }
+
+        // POST: estudiantesSets/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Idestudiantes,materia,nombre")] estudiantesSet estudiantesSet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudiantesSet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estudiantesSet);
+        }
+
+        // GET: estudiantesSets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            estudiantesSet estudiantesSet = db.estudiantesSet.Find(id);
+            if (estudiantesSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudiantesSet);
+        }
+
+        // POST: estudiantesSets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            estudiantesSet estudiantesSet = db.estudiantesSet.Find(id);
+            db.estudiantesSet.Remove(estudiantesSet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GitHub/GitHub/Controllers/eventosSetsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GitHub.Models;
+
+namespace GitHub.Controllers
+{
+    public class eventosSetsController : Controller
+    {
+        private GHBbdEntities db = new GHBbdEntities();
+
+        // GET: eventosSets
+        public ActionResult Index()
+        {
+            return View(db.eventosSet.ToList());
+        }
+
+        // GET: eventosSets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventosSet eventosSet = db.eventosSet.Find(id);
+            if (eventosSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosSet);
+        }
+
+        // GET: eventosSets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: eventosSets/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Ideventos,evento,hora,fecha")] eventosSet eventosSet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.eventosSet.Add(eventosSet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eventosSet);
+        }
+
+        // GET: eventosSets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventosSet eventosSet = db.eventosSet.Find(id);
+            if (eventosSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosSet);
+        }
+
+        // POST: eventosSets/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Ideventos,evento,hora,fecha")] eventosSet eventosSet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eventosSet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eventosSet);
+        }
+
+        // GET: eventosSets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            eventosSet eventosSet = db.eventosSet.Find(id);
+            if (eventosSet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eventosSet);
+        }
+
+        // POST: eventosSets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            eventosSet eventosSet = db.eventosSet.Find(id);
+            db.eventosSet.Remove(eventosSet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
